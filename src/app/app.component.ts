3d63d35070f2b4696fb753ca5ce1ee2d6e8fdb35
Arjun Paliath <arjun@freshdesk.com>
2017-07-06 14:23:12 +0530

    Hotline Code
    Added Hotline buttons and enabled it.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,13 +8,15 @@ import { ListPage } from '../pages/list/list';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 
+declare var window: any;
 
 @Component({
   templateUrl: 'app.html'
 })
 export class MyApp {
   @ViewChild(Nav) nav: Nav;
-
+  hotline: any;
+  pushNotification: any;
   // make HelloIonicPage the root (or first) page
   rootPage = HelloIonicPage;
   pages: Array<{title: string, component: any}>;
@@ -35,13 +37,78 @@ export class MyApp {
   }
 
   initializeApp() {
+    var self = this;
     this.platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       this.statusBar.styleDefault();
       this.splashScreen.hide();
+      self.initHotline();
+      self.initPushNotifications();
     });
   }
+
+  initHotline() {
+    console.log('Init Hotline');
+    console.log(window);
+    this.hotline  = window.Hotline;
+    var self = this;
+    self.hotline.init({
+        appId: "<Your App Id>",
+        appKey: "<Your App Key>"
+    },
+    function(success){
+    console.log("Hotline successfully intialized");
+    self.initPushNotifications();
+    self.hotline.updateUser({
+        "name" : "John Doe",
+        "email" : "john@doe.com",
+        "countryCode" : "+91",
+        "externalId" : "john.doe",
+        "phoneNumber" : "8000080000"
+      });
+    });
+  }
+
+  initPushNotifications() {
+    console.log("Init Push Notifications");
+
+    var push = window.PushNotification;
+    this.pushNotification = push.init({
+        android: {
+            senderID: "<Sender ID>"
+          },
+          ios: {
+            alert: "true",
+            badge: false,
+            sound: "true"
+          },
+          windows: {}
+    }, function() {
+        console.log("push Notification sucessfully instialized");
+    });
+
+    var self = this;
+    this.pushNotification.on('registration',function(data) {
+        console.log("updating deviceID"); // This console is getting executed
+        // self.hotline.updatePushNotificationToken(self.deviceID);
+        self.hotline.updatePushNotificationToken(data.registrationId);
+    });
+
+    this.pushNotification.on('notification',function(data) {
+    // The below code block is not executing when I closed my app
+    // And sending messages from web.hotline.io
+    console.log("Notification came");
+    self.hotline.isHotlinePushNotification(data.additionalData, function(success, isHotlineNotif) {
+      console.log("push notification yet to success");
+        if( success && isHotlineNotif ) {
+            console.log("push notifiction success");
+            self.hotline.handlePushNotification(data.additionalData);
+        }
+      });
+    });
+  }
+
 
   openPage(page) {
     // close the menu when clicking a link from the menu
--- src/pages/hello-ionic/hello-ionic.ts
+++ src/pages/hello-ionic/hello-ionic.ts
@@ -1,11 +1,28 @@
 import { Component } from '@angular/core';
+import { Platform } from 'ionic-angular';
+
+declare var window: any;
 
 @Component({
   selector: 'page-hello-ionic',
   templateUrl: 'hello-ionic.html'
 })
-export class HelloIonicPage {
-  constructor() {
 
+export class HelloIonicPage {
+  hotline: any;
+  constructor(public platform: Platform) {
+    platform.ready().then(() => {
+     console.log('loaded');
+     console.log(window);
+     this.hotline = window.Hotline;
+    });
+  }
+  openSupport() {
+    console.log("Opening Support Conversation");
+    this.hotline.showConversations();
+  }
+  openFAQs() {
+    console.log("Opening Support Conversation");
+    this.hotline.showFAQs();
   }
 }
